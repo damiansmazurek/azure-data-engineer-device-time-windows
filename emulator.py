db9e2295daea7bc01e9867f86418e224ee31cab3
Damian <damian.s.mazurek@gmail.com>
2020-03-04 19:45:39 +0100

    Emulator

---
+++ emulator.py
@@ -0,0 +1,22 @@
+import uuid
+import time
+from azure.eventhub import EventHubProducerClient, EventDataBatch, EventData
+
+class DeviceEmulator:
+    def __init__(self, connectionstring, eventhub_name):
+        self.client = EventHubProducerClient.from_connection_string(connectionstring, eventhub_name=eventhub_name )
+        # Create Event Hubs client
+        self.deviceId = uuid.uuid4()
+
+    def send_data(self, data):
+        # Send message to eventhub
+        self.send_batch_data([data])
+
+    def send_batch_data(self, dataBatch):
+        # Send batch data to eventhub
+        batchData = EventDataBatch()
+        for item in dataBatch:
+            message = {"deviceid": self.deviceId, "timestamp": time.time(), "data": item}
+            batchData.add(EventData(message))
+        self.client.send_batch(batchData)
+        
