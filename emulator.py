7101c8e9e5545cabcaeba4b1587b5d160614990a
Damian <damian.s.mazurek@gmail.com>
2020-03-25 18:00:52 +0100

    Build trigger test

--- emulator.py
+++ emulator.py
@@ -1,7 +1,7 @@
 import uuid
 import time
 from azure.eventhub import EventHubProducerClient, EventDataBatch, EventData
-
+ 
 class DeviceEmulator:
     def __init__(self, connectionstring, eventhub_name):
         self.client = EventHubProducerClient.from_connection_string(connectionstring, eventhub_name=eventhub_name )
